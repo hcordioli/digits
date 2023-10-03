594f734dd601e027816890faa5bb0d3eaa5d24b8
Humberto Cordioli <hcordioli@gmail.com>
2023-10-03 10:55:12 -0300

    Add files via upload

---
+++ digits/Home.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import tensorflow as tf
+
+st.set_page_config(
+    page_title="Handwriten Digit Recognizer",
+    page_icon="🧠",
+)
+
+if 'model_loaded' not in st.session_state:
+    st.session_state['model_loaded'] = False
+        
+st.write("# Welcome to Handwriten Digit Recognizer! 🧠")
+#st.sidebar.success("Home")
+
+st.markdown(
+    """
+    #Handwriten Digit Recognizer
+"""
+)
+
+load_model = st.button("Load Model")
+if load_model:
+    try:
+        fname = "./data/digit_recognizer.keras"
+        hdr_model = tf.keras.saving.load_model(fname)
+        st.session_state['model_loaded'] = True
+        if 'hdr_model' not in st.session_state:
+            st.session_state['hdr_model'] = hdr_model
+        st.write ("Now you can predict an image")
+    except OSError:
+        err_msg = "Could not find model file: "+ fname
+        st.write (err_msg)
